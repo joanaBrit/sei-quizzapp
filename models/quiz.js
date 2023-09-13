55e319b334509b48c8258b0cd7c02936b08a9389
Nathalie M Kirch <99728936+NLie2@users.noreply.github.com>
2023-09-13 18:08:19 +0200

    add quiz model

---
+++ models/quiz.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+import questionSchema from './question.js'
+
+// * SCHEMAS
+
+// QUESTION
+const questionSchema = new mongoose.schema({
+  question: { type: String, required: true, unique: true }, 
+  answer: { type: String, requried: true, unique: true }, 
+  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+})
+
+
+// QUIZ
+const quizSchema = new mongoose.Schema({
+  title: { type: String, required: true, maxlength: 30}, 
+  questions: [ questionSchema ]
+
+  // ! BONUS 
+  // add max number of questions
+  // add highscore 
+  // add functionality for duplicate questions 
+  // add functionality to create new quiz (only available to admins)
+
+})
+
+
+// * EXPORT MODEL
+export default mongoose.model('Quiz', quizSchema)
