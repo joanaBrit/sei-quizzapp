305cfd1806d10a935ee8688b1db96bacb9fc0b76
Nathalie <nathalie.kirch.nk@gmail.com>
2023-09-14 15:14:53 +0200

    fix linting issues in seed quizzes

--- db/data/quizzes.js
+++ db/data/quizzes.js
@@ -1,22 +1,22 @@
 export default [
-  { title: "React", 
+  { title: 'React', 
     questions: [ 
-      { question: "A question about react 1 ", 
-        answer: "An answer about react 1", 
+      { question: 'A question about react 1 ',
+        answer: 'An answer about react 1', 
       }, 
-      { question: "A question about react 2 ", 
-        answer: "An answer about react 2", 
-      },
-    ]
+      { question: 'A question about react 2 ', 
+        answer: 'An answer about react 2', 
+      }
+    ],
   }, 
-  { title: "Node", 
+  { title: 'Node', 
     questions: [ 
-      { question: "A question about node 1 ", 
-        answer: "An answer about node 1", 
+      { question: 'A question about node 1 ', 
+        answer: 'An answer about node 1', 
       }, 
-      { question: "A question about node 2 ", 
-        answer: "An answer about node 2", 
-      },
-    ]
+      { question: 'A question about node 2 ', 
+        answer: 'An answer about node 2', 
+      }
+    ],
   }
 ]
