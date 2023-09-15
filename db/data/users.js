43509418235030fa4f2103fdc77114cc5b34e06b
Aleale999 <alebatti2710gmail.com>
2023-09-15 10:57:10 +0200

    Adding update questions

--- db/data/users.js
+++ db/data/users.js
@@ -3,18 +3,15 @@ export default [
     username: 'Nathalie', 
     email: 'nathalie@example.com', 
     password: 'pass',
-    passwordConfirmation: 'pass',
   },
   {
     username: 'Alessio', 
     email: 'alessio@example.com', 
     password: 'pass',
-    passwordConfirmation: 'pass',
   },
   {
     username: 'Joana', 
     email: 'joana@example.com', 
     password: 'pass',
-    passwordConfirmation: 'pass',
   }
 ]
