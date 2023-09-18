1bcde72afd3e323f88fa77ca877c1d8e3bcf2857
Nathalie <nathalie.kirch.nk@gmail.com>
2023-09-18 11:11:47 +0200

    add icon key to quiz Schema

--- models/quiz.js
+++ models/quiz.js
@@ -6,14 +6,15 @@ import mongoose from 'mongoose'
 const questionSchema = new mongoose.Schema({
   question: { type: String, required: true, unique: true }, 
   answer: { type: String, requried: true, unique: true }, 
-  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }, 
 })
 
 
 // QUIZ
 const quizSchema = new mongoose.Schema({
-  title: { type: String, required: true, maxlength: 30}, 
-  questions: [ questionSchema ]
+  title: { type: String, required: true, maxlength: 30 }, 
+  questions: [ questionSchema ], 
+  icon: { type: String },
 
   // ! BONUS 
   // add max number of questions
