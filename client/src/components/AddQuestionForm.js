1695c9470cdad46d3a1c9434a2d8a914b457bcc1
Nathalie <nathalie.kirch.nk@gmail.com>
2023-09-18 23:14:49 +0200

    add basic form to client for addng questions

--- client/src/components/AddQuestionForm.js
+++ client/src/components/AddQuestionForm.js
@@ -4,6 +4,20 @@ import Button from 'react-bootstrap/Button'
 
 import NavBar from './NavBar'
 
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function handleSubmit(){
+  console.log('handle submit')
+  //I guess this is where the post request should be sent 
+}
+function handleChange(){
+  console.log('handle change')
+  //I guess this is where the post request should be sent 
+}
+
 export default function AddQuestionForm( { username, quiz }) {
   return (
     <section>
@@ -20,12 +34,20 @@ export default function AddQuestionForm( { username, quiz }) {
           <h1 className="title text-center text-uppercase mb-5">Add a Question to {/* quiz.id */ } </h1>
 
         </section>
-        <section>
-          <p>Feeling inspired?</p>
-          <Link to='/quizzes/:quizId'>
-            <Button type='button' className='btn btn-sm btn-blue btn-block'>Add some Questions</Button>
-          </Link>
-        </section>
+        <Container>
+          <Row>
+            <Col as="form" xs={{ span: 8, offset: 2 }} md={{ span: 6, offset: 3 }} onSubmit={handleSubmit} autoComplete='off'>
+
+              <label hidden htmlFor='question'>question</label>
+              <input type='text' name='question' placeholder='Your question' onChange={handleChange} id='questionId'
+              />
+              <input type='text' name='question' placeholder='Correct answer' onChange={handleChange} id='questionId'
+              />              
+              {/* {errors && <p className='text-warning bold text-center mt-5'>{errors}</p>} */}
+              <button type="submit">Submit</button>
+            </Col>
+          </Row>
+        </Container>
       </main>
     </section>
 
