0ea5cbebf13425cd2304b1615f30267515e50617
Nathalie <nathalie.kirch.nk@gmail.com>
2023-09-19 23:27:11 +0200

    add more elaborate seed

--- db/data/quizzes.js
+++ db/data/quizzes.js
@@ -1,11 +1,11 @@
 export default [
   { title: 'React', 
     questions: [ 
-      { question: 'A question about react 1 ',
-        answer: 'An answer about react 1', 
+      { question: 'Hooks',
+        answer: '', 
       }, 
-      { question: 'A question about react 2 ', 
-        answer: 'An answer about react 2', 
+      { question: 'Routes', 
+        answer: '', 
       }
     ],
     icon: '../assets/logo-react.png',
@@ -20,5 +20,82 @@ export default [
       }
     ],
     icon: '../assets/logo-node.png',
+  },
+  { title: 'Git', 
+    questions: [ 
+    ],
+    icon: '',
+  },
+  { title: 'CSS', 
+    questions: [ 
+    ],
+    icon: '',
+  },
+  { title: 'JavaScript', 
+    questions: [ 
+      { question: 'Array functions', 
+        answer: '', 
+      }, 
+      { question: 'Ternaries', 
+        answer: '', 
+      }, 
+      { question: 'Deconstructing', 
+        answer: '', 
+      }
+    ],
+    icon: '',
+  },
+  { title: 'Development practices', 
+    questions: [ 
+      { question: 'What is test-driven development?', 
+        answer: 'Test-driven development (TDD) is a software development process relying on software requirements being converted to test cases before software is fully developed, and tracking all software development by repeatedly testing the software against all test cases. This is as opposed to software being developed first and test cases created later.', 
+      }, 
+      { question: 'Why is it good practice to use semantic HTML?', 
+        answer: 'Semantic HTML gives meaning and structure to web content. It helps search engines and assistive technologies understand content. Semantic HTML is not a direct ranking factor but improves SEO and accessibility. Using semantic HTML creates an optimal experience for all users.', 
+      }
+    ],
+    icon: '',
+  },
+  { title: 'History', 
+    questions: [ 
+      { question: 'Who was the original developer of the z shell??', 
+        answer: 'Paul Falstad, who named the shell after his Yale professor Zhong Shao (Zsh)', 
+      }, 
+      { question: 'What is special about the game Rollercoaster Tycoon, developed by Chris Sawyer?', 
+        answer: 'The game was developed in 1990s and was extremely elegant for its time. The game was developed in a small village near Dunblane over the course of two years. Sawyer wrote 99% of the code for RollerCoaster Tycoon in assembly code, (an extremely low-level programming language) for the Microsoft Macro Assembler, with the remaining one percent written in C. ', 
+      }
+    ],
+    icon: '',
+  },
+  { title: 'HTTP Requests', 
+    questions: [ 
+    ],
+    icon: '',
+  },
+  { title: 'Express', 
+    questions: [ 
+    ],
+    icon: '',
+  },
+  { title: 'MongoDB', 
+    questions: [ 
+      { question: 'Mongoose', 
+        answer: '', 
+      }, 
+      { question: 'The Mongosh Shell', 
+        answer: '', 
+      }
+    ],
+    icon: '../assets/logo-node.png',
+  },
+  { title: 'Python', 
+    questions: [ 
+    ],
+    icon: '',
+  },
+  { title: 'Django', 
+    questions: [ 
+    ],
+    icon: '',
   }
 ]
